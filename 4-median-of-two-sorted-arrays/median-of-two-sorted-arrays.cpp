069b9ae921a1d2d41fa6be3768e27ba416e1ddc1
Maharshi Dindoliwala <maharshi1731@gmail.com>
2025-09-01 18:57:10 +0530

    Time: 4 ms (21.87%) | Memory: 95.9 MB (26.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> final_arr;
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0;
+
+        // Merge the two arrays
         while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] > nums2[j]) {
-                final_arr.push_back(nums2[j]);
-                j++;
+            if (nums1[i] < nums2[j]) {
+                final_arr.push_back(nums1[i++]);
             } else {
-                final_arr.push_back(nums1[i]);
-                i++;
+                final_arr.push_back(nums2[j++]);
             }
-            
         }
-        while(i<nums1.size()){
-            final_arr.push_back(nums1[i]);
-            i++;
-        
-        }
-        while(j<nums2.size()){
-            final_arr.push_back(nums2[j]);
-            j++;
-        
-        }
-        int mid = (final_arr.size()-1) /2 ;
-        double median =0;
 
-        if(final_arr.size()%2 == 0){
-            median = final_arr[mid] + final_arr[mid+1];
-            median/=2;
+        // Add remaining elements
+        while (i < nums1.size()) {
+            final_arr.push_back(nums1[i++]);
         }
-        else median = final_arr[mid];
-        
-        return median;
+
+        while (j < nums2.size()) {
+            final_arr.push_back(nums2[j++]);
+        }
+
+        // Find the median
+        int n = final_arr.size();
+        if (n % 2 == 0) {
+            return (final_arr[n/2 - 1] + final_arr[n/2]) / 2.0;
+        } else {
+            return final_arr[n/2];
+        }
     }
-
 };
