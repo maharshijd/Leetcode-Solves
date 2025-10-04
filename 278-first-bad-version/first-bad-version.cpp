be7da475ace3fb813dc40dc78cba41c12de35fc0
Maharshi Dindoliwala <maharshi1731@gmail.com>
2025-10-04 21:43:29 +0530

    Time: 3 ms (17.75%) | Memory: 7.8 MB (69.17%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,21 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left = 1,right = n;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if(isBadVersion(mid) == true){
-                while(isBadVersion(mid) == true){
-                    if(mid == 1) return 1;
-                    else mid = mid-1;
-                }
-                return mid+1;
-            }
-            else{
-                left = mid+1;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return -1;
+        return left;
     }
-
 };
