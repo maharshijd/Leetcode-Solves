3108f8cf86467214c151dd3e9f593f7048d5b2b2
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-03-27 15:09:04 +0530

    Time: 0 ms (100.00%) | Memory: 96.1 MB (9.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> final_arr;
         int i = 0, j = 0;
 
-        // Merge the two arrays
         while (i < nums1.size() && j < nums2.size()) {
             if (nums1[i] < nums2[j]) {
                 final_arr.push_back(nums1[i++]);
@@ -12,8 +11,6 @@ public:
                 final_arr.push_back(nums2[j++]);
             }
         }
-
-        // Add remaining elements
         while (i < nums1.size()) {
             final_arr.push_back(nums1[i++]);
         }
@@ -22,7 +19,6 @@ public:
             final_arr.push_back(nums2[j++]);
         }
 
-        // Find the median
         int n = final_arr.size();
         if (n % 2 == 0) {
             return (final_arr[n/2 - 1] + final_arr[n/2]) / 2.0;
