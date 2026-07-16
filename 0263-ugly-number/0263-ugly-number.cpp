e0f025848c8ec8efa0704dcee25c7b05013976ac
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-07-16 14:01:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (77.24%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        while(n%2==0 || n%3==0 || n%5==0){
+            if(n%2 == 0) n/=2;
+            if(n%3==0) n/=3;
+            if(n%5==0) n/=5;
+        }
+        n = abs(n);
+        if(n!=1) return false;
+        return true;
+    }
+};
